77d87bc2005b7bf0fff20aff1d56fae610ab579c
Lai Nara <lay.channara@gmail.com>
2018-06-17 09:57:52 +0700

    Restructure project.

---
+++ Example/NaturalVoice/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NaturalVoice
+//
+//  Created by LayChannara on 06/17/2018.
+//  Copyright (c) 2018 LayChannara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
--- Example/Pods/Target Support Files/Pods-NaturalVoice_Example/Pods-NaturalVoice_Example-umbrella.h
+++ Example/Pods/Target Support Files/Pods-NaturalVoice_Example/Pods-NaturalVoice_Example-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double AlamofireVersionNumber;
-FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
+FOUNDATION_EXPORT double Pods_NaturalVoice_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_NaturalVoice_ExampleVersionString[];
 
