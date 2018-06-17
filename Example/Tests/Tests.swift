92205ee5c24693420cc86af417c8f089dad922ed
Lai Nara <lay.channara@gmail.com>
2018-06-17 11:27:11 +0700

    ...

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -7,9 +7,9 @@ class Tests: QuickSpec {
     override func spec() {
         context("Strategy") {
             beforeEach {
-                VoiceRecordStrategy.apiKey = ""
-                VoiceRecordStrategy.language = nil
-                VoiceRecordStrategy.maxRecordDuration = 65
+                VoiceRecordStrategy.apiKey = "Your Api Key"
+                VoiceRecordStrategy.language = VoiceLanguageManager.shared.getLanguage(bcp47Code: "en-US")
+                VoiceRecordStrategy.maxRecordDuration = 30
                 VoiceRecordStrategy.maxRecordDurationPolicy = .sendImmediately
             }
             it("ApiKey") {
@@ -20,6 +20,25 @@ class Tests: QuickSpec {
             }
             it("MaxRecordDuration") {
                 expect(VoiceRecordStrategy.maxRecordDuration) <= 60
+            }
+        }
+        
+        context("Language") {
+            
+            var languages: [VoiceLanguage] = []
+            var language: VoiceLanguage?
+            
+            beforeEach {
+                languages = VoiceLanguageManager.shared.supportLanguages
+                language = VoiceLanguageManager.shared.getLanguage(bcp47Code: "en-US")
+            }
+            
+            it("Count") {
+                expect(languages.count).toNot(equal(0))
+            }
+            
+            it("Language") {
+                expect(language).toNot(beNil())
             }
         }
     }
